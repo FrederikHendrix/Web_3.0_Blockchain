dd8b827b803e02b1cb8f04aef0c2244c15151256
PeopDePelikaan <35820277+PeopDePelikaan@users.noreply.github.com>
2022-10-31 15:13:35 +0100

    Final Commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     goerli : {
       url : 'https://eth-goerli.g.alchemy.com/v2/lPqOB1hduwePkUJt_m9Yh5m4eLXwmVWW',
-      accounts: ['91ec3c2d94712c0b30349577735db17fe644adfe62e41851edad51329152a916']
+      accounts: ['']
     }
   }
 }
